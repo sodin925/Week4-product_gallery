716c1d4feb2246491d039e875fec0c4097863215
sodin925 <t0975082751@gmail.com>
2018-07-01 13:23:35 +0800

    2

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function() {
-    gulp.src('./Week4-product_gallery/*.scss')//抓
-        .pipe(sass())
-        .pipe(gulp.dest('./Week4-product_gallery/CSS/'))//建立
+gulp.task('sass', function () {
+  gulp.src('./*.scss')//抓
+    .pipe(sass())
+    .pipe(gulp.dest('./CSS/'))//建立
 });
 
-gulp.task('default', ['sass'], function() {
-    gulp.watch('./*.scss', ['sass']);
+gulp.task('default', ['sass'], function () {
+  gulp.watch('./*.scss', ['sass']);
 })
